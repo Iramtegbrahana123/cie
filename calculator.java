8fc38322f0a1b5f6e4b582c758ee242c58fbefb0
Bindu275 <bindu46359@gmail.com>
2025-04-15 15:06:05 +0530

    helloworld

---
+++ calculator.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        
+        System.out.println("Addition="+(a+b));
+        System.out.println("Substraction="+(a-b));
+        System.out.println("Multiplication="+(a*b));
+        System.out.println("division="+(a/b));
+        System.out.println("Modulus="+(a%b));
+
+    }
+}
